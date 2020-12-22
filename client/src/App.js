abc9bfa1f6494d8cf0c49a0b2edd6d4fe26a7901
SjostromJesper <j.sjostrom@live.se>
2020-12-22 15:17:43 +0100

    chcekc

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,26 @@ import Map from "./components/Map/Map.js";
 import LogText from "./components/LogText/LogText.js";
 import Loadbar from "./components/Loadbar/Loadbar.js";
 
+import io from 'socket.io-client'
 
-function App() {
+let socket;
+
+const App = () => {
+
+    const verify = (tokenId) => {
+        console.log(tokenId)
+        socket = io.connect("http://localhost:3001")
+    }
+
+
   return (
     <div className="App">
-      <Inventory/>
-      <Log/>
-      <Map/>
-      <LogText/>
-      <Loadbar/>
+        <button onClick={() => {verify()}}>click me!</button>
+        <Inventory/>
+        <Log/>
+        <Map/>
+        <LogText/>
+        <Loadbar/>
     </div>
   );
 }
